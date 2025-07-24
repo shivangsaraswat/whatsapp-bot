c1b9693d9e000feaca21327dd4ac86db3e5efc53
Shivang Saraswat <63785853+shivangsaraswat@users.noreply.github.com>
2025-07-24 14:33:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,40 +2,19 @@ module.exports = {
   groups: [
     {
       groupName: "Verification",
-      groupId: '120363421079207775@g.us',
+      groupId: '',// groupId
       googleSheets: [
-        '1OG4x1T6eTsUYqDm5KFHiBm8PJiBMU3yQ9Zn3Gdw67RU'
+        ''// googleSheets
       ]
     }
   ],
-  adminNumber: '+919876543210',
-  allowedAllAdmins: ['+919876543210', '+919812345678'],
+  adminNumber: '',//  adminNumber
+  allowedAllAdmins: [''],// allowedAllAdmins
   allowedAllGroups: [
-    '120363332203849781@g.us',
-    '919941663899-1633177842@g.us',
-    '120363348123092213@g.us',
-    '120363421150088277@g.us',
-    '120363347282322111@g.us',
-    '120363332102598014@g.us',
-    '120363421079207775@g.us',
-    '120363331887862553@g.us',
-    '120363319130577663@g.us',
-    '120363332444266551@g.us',
-    '120363327066859037@g.us',
-    '120363417682582361@g.us',
-    '120363345961186677@g.us',
-    '120363331731691719@g.us',
-    '120363349152263421@g.us',
-    '120363349370800191@g.us',
-    '120363332551124006@g.us',
-    '120363351329078354@g.us',
-    '120363409892706234@g.us',
-    '120363394532156100@g.us',
-    '120363316898386527@g.us',
-    '120363334027571387@g.us',
-    '120363349805629689@g.us'
+    '',// allowedAllGroups ids
+    
   ],
-  excludedFromAllTag: ['+919876543210'],
+  excludedFromAllTag: [''],//excludedFromAllnumber
   google: {
     credentialsPath: 'credentials.json', // Place your JSON file in the project root or update the path
     tokenPath: undefined // If you use a token file, set the path here
